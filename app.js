beedada2980a77ede3739b3782ca226575b047f1
Henry Patterson <henrycpatterson92@gmail.com>
2022-01-10 17:54:09 -0500

    api

---
+++ app.js
@@ -0,0 +1,4 @@
+const DOMAIN = 'https://api.nasa.gov/planetary/apod?';
+const API_KEY = 'c9euw6GQ4toPdwuHs7UdScoXCi89D04WrVIujZ2H'
+const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&`;
+const searQueries=[]
