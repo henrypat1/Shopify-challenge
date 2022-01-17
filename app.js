34d65bd1b344371345678982e9deb12ecb04e0e5
Henry Patterson <henrycpatterson92@gmail.com>
2022-01-17 16:53:26 -0500

    search

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const DOMAIN = 'https://api.nasa.gov/planetary/apod';
-const API_KEY = 'c9euw6GQ4toPdwuHs7UdScoXCi89D04WrVIujZ2H'
+const API_KEY = '76AJY5jTzJlxYCjVZbgKF2dRSS7oaTDk3GHtpbr7'
 const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&`;
+// const BASE_URL = 'https://api.nasa.gov/planetary/apod?api_key=c9euw6GQ4toPdwuHs7UdScoXCi89D04WrVIujZ2H'
 const searQueries = []
 
 let text = document.querySelector('#search')
@@ -27,9 +28,9 @@ async function getPictures(input){
       h2.append(nasaDiv)
       nasaDiv.classList.add('picture')
       nasaDiv.textContent = nasaList[i].Title
-      // nasaPicture.src = `${nasaList[i].Url}`
-      // nasaDiv.textContent = nasaList[i].Explanation
-      // nasaDiv.appendChild(nasaPicture)
+      nasaPicture.src = `${nasaList[i].Url}`
+      nasaDiv.textContent = nasaList[i].Explanation
+      nasaDiv.appendChild(nasaPicture)
       let result = document.querySelector('.nasa-pictures')
       result.appendChild(nasaDiv)
       i++
