d955537375e316ad4f318a27c962bddd47bafb6c
Henry Patterson <henrycpatterson92@gmail.com>
2022-01-19 13:34:05 -0500

    search

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const DOMAIN = 'https://api.nasa.gov/planetary/apod';
 const API_KEY = '76AJY5jTzJlxYCjVZbgKF2dRSS7oaTDk3GHtpbr7'
 const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&`;
 // const BASE_URL = 'https://api.nasa.gov/planetary/apod?api_key=c9euw6GQ4toPdwuHs7UdScoXCi89D04WrVIujZ2H'
-// const searQueries = []
+const searQueries = []
 
 let text = document.querySelector('#search')
 let input = document.querySelector('#blank')
@@ -16,13 +16,23 @@ function storeText() {
   console.log(input)
 }
 
-// form.addEventListener('submit', (e) => {
-//   e.preventDefault()
-//   const input = document.querySelector('title').value
-//   console.log(input)
-// })
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
 async function getPictures(input){
   try {
+    // const DOMAIN = 'https://api.nasa.gov/planetary/apod';
+    // const API_KEY = '76AJY5jTzJlxYCjVZbgKF2dRSS7oaTDk3GHtpbr7';
+    // const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&`;
+
     const response = await axios.get(`${BASE_URL}s=${input.value}`)
     let nasaList = response.data.Search
     console.log(nasaList)
@@ -33,9 +43,9 @@ async function getPictures(input){
       let nasaPicture = document.createElement('img')
       h2.append(nasaDiv)
       nasaDiv.classList.add('picture')
-      nasaDiv.textContent = nasaList[i].Date
-      // nasaPicture.src = `${nasaList[i].Url}`
-      // nasaDiv.textContent = nasaList[i].Explanation
+      nasaDiv.textContent = nasaList.title
+      nasaPicture.src = `${nasaList[i].Url}`
+      nasaDiv.textContent = nasaList[i].Explanation
       nasaDiv.appendChild(nasaPicture)
       let result = document.querySelector('.nasa-pictures')
       result.appendChild(nasaDiv)
